1da184735f1d1c067432021e1baa644a0ea68fc8
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-12 22:53:01 -0400

    Feat: Display the todos items
    Displayed all todos items with `Edit` and `Delete` button for each item.
    Currently the `Edit` and `Delete` button don't perform required actions.

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,30 @@
 import React from "react";
+import TodosList from './todos-list';
+
+const todos = [
+  {
+    task: 'Cook Dinner',
+    isCompleted: false
+  },
+  {
+    task: 'Udacity Checkin',
+    isCompleted: true
+  }
+];
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos
+    };
+  }
+
   render() {
     return (
       <div>
         <h1>Todos</h1>
+        <TodosList todos={this.state.todos}/>
       </div>
     );
   }
