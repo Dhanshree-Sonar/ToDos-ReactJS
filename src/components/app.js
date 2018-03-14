f57dbea7f3dedac7240722ce6e9e0b1677b9416b
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-13 21:56:47 -0400

    Feat: Implement delete functionality

--- src/components/app.js
+++ src/components/app.js
@@ -31,6 +31,7 @@ export default class App extends React.Component {
           // Pass the functions to TodosList
           toggleTask={this.toggleTask.bind(this)}
           saveTask={this.saveTask.bind(this)}
+          deleteTask={this.deleteTask.bind(this)}
         />
       </div>
     );
@@ -54,6 +55,11 @@ export default class App extends React.Component {
   saveTask(oldTask, newTask) {
     const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
     foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos });
+  }
+
+  deleteTask(task) {
+    _.remove(this.state.todos, todo => todo.task === task);
     this.setState({ todos: this.state.todos });
   }
 }
